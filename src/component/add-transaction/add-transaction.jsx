11886727a5d5beef43dfa91c359bc0b19bc1fb60
suleyman2004 <128624857+suleyman2004@users.noreply.github.com>
2024-04-29 15:30:12 +0700

    Transaction

---
+++ src/component/add-transaction/add-transaction.jsx
@@ -0,0 +1,94 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context/global_context";
+
+const AddTransaction = ({ isOpen, onClose }) => {
+  const { formData, setFormData, value, setValue, handleFormSubmit } =
+    useContext(GlobalContext);
+
+  function handleFormChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleFormSubmit(formData);
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New Transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Enter Description</FormLabel>
+              <Input
+                placeholder="Enter Transaction description"
+                name="description"
+                type="text"
+                onChange={handleFormChange}
+              ></Input>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Enter Amount</FormLabel>
+              <Input
+                placeholder="Enter Transaction amount"
+                name="amount"
+                type="number"
+                onChange={handleFormChange}
+              ></Input>
+            </FormControl>
+            <RadioGroup mt={"5"} value={value} onChange={setValue}>
+              <Radio
+                checked={formData.type === "income"}
+                value="income"
+                colorScheme="blue"
+                name="type"
+                onChange={handleFormChange}
+              >
+                Income
+              </Radio>
+              <Radio
+                checked={formData.type === "expense"}
+                value="expense"
+                colorScheme="red"
+                name="type"
+                onChange={handleFormChange}
+              >
+                Expense
+              </Radio>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter onClick={onClose}>
+            <Button mr={"4"}>Cancel</Button>
+            <Button onClick={onClose} type="submit">
+              Add
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddTransaction;
---
+++ src/component/main/main.jsx
@@ -0,0 +1,60 @@
+import { Button, Flex, Heading, useDisclosure } from "@chakra-ui/react";
+import React, { useContext, useEffect } from "react";
+import ExpemseView from "../expense-view/expemse-view";
+import Summary from "../summary/summary";
+import { GlobalContext } from "../../context/global_context";
+
+const Main = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    totalExpense,
+    setTotalExpense,
+    totalIncome,
+    setTotalIncome,
+    allTransactions,
+  } = useContext(GlobalContext);
+
+  useEffect(() => {
+    let income = 0;
+    let expense = 0;
+
+    allTransactions.forEach((item) => {
+      item.type === "income"
+        ? (income = income + parseFloat(item.amount))
+        : (expense = expense + parseFloat(item.amount));
+
+      setTotalExpense(expense);
+      setTotalIncome(income);
+    },[allTransactions]);
+  });
+
+  return (
+    <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
+        <Heading
+          color={"blue.400"}
+          display={["none", "block", "block", "block", "block"]}
+        >
+          Expense Tracker
+        </Heading>
+        <Flex alignItems={"center"}>
+          <Button onClick={onOpen} bg={"blue.300"} color={"black"} ml={"4"}>
+            Add New Transaction
+          </Button>
+        </Flex>
+      </Flex>
+      <Summary totalExpense={totalExpense} totalIncome={totalIncome} isOpen={isOpen} onClose={onClose} />
+      <Flex
+        w={"full"}
+        alignItems={"flex-start"}
+        justifyContent={"space-evenly"}
+        flexDirection={["column", "column", "column", "row", "row"]}
+      >
+        <ExpemseView type={"expense"} data={allTransactions.filter((item)=> item.type==="expense")}/>
+        <ExpemseView type={"income"} data={allTransactions.filter((item)=> item.type==="income")} />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Main;
